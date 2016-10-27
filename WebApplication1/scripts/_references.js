a32f485bcc5bdb34ebe7bcba8410cf4f334cb753
tom <tom.chen@kingwaytek.com>
2016-10-27 11:55:59 +0800

    新增 angularjs 專案結構

---
+++ WebApplication1/scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="app/app.js" />
+/// <reference path="lib/ai.0.22.9-build00167.js" />
+/// <reference path="lib/angular/angular.js" />
+/// <reference path="lib/angular/angular-mocks.js" />
+/// <reference path="lib/jquery/jquery-3.1.1.js" />
+/// <reference path="lib/jquery/jquery-3.1.1.slim.js" />
